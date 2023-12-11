35f26d4985ab68ebd4cf44fa61321b434e395f1c
Tito2point0 <defconx00@gmail.com>
2023-12-10 21:09:35 -0500

    back to good message

--- api/server.js
+++ api/server.js
@@ -9,11 +9,6 @@ server.use('*', (req, res) => {
         message: 'your request was not completed '
     })
 })
-server.use((err, req, res, next) => {
-    res.status(500).json({
-        message: 'something went wrong'
-    })
-})
 
 
 module.exports = server;
