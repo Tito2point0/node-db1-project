2ea57a5b51e83aa9a18cfac8620cb7736ab1b4ab
Tito2point0 <defconx00@gmail.com>
2023-12-20 15:06:44 -0500

    endpoints wired and working with error handling

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,26 +1,56 @@
 const router = require('express').Router()
 
+
+
 router.get('/', (req, res, next) => {
+  try {
+   res.json([{}, {}])
+  } catch(err) {
+    next({ status: 422, message: 'something went wrong'})
+  }
   // DO YOUR MAGIC
 })
 
 router.get('/:id', (req, res, next) => {
+  try {
+    res.json('get accounts by id ')
+  } catch(err) {
+    next(err)
+  }
   // DO YOUR MAGIC
 })
 
 router.post('/', (req, res, next) => {
+  try {
+    res.json('post account ')
+  } catch(err) {
+    next(err)
+  }
   // DO YOUR MAGIC
 })
 
 router.put('/:id', (req, res, next) => {
+  try {
+    res.json('update accounts')
+  } catch(err) {
+    next(err)
+  }
   // DO YOUR MAGIC
 });
 
 router.delete('/:id', (req, res, next) => {
+  try {
+    res.json('delete accounts')
+  } catch(err) {
+    next(err)
+  }
   // DO YOUR MAGIC
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message
+  }) 
   // DO YOUR MAGIC
 })
 
--- api/server.js
+++ api/server.js
@@ -2,7 +2,11 @@ const express = require("express");
 
 const server = express();
 
+const accountsRouter = require('./accounts/accounts-router.js')
+
 server.use(express.json());
+
+server.use('/api/accounts', accountsRouter)
 
 server.use('*', (req, res) => {
     res.status(404).json({
