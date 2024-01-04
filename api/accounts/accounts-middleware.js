97407b555b736a7eb88ed38ee5fc7146361ad3b8
Tito2point0 <defconx00@gmail.com>
2024-01-03 19:45:59 -0500

    budget and names sasd paths

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,30 +1,41 @@
-const Account = require('./accounts-model')
+const Account = require("./accounts-model");
 exports.checkAccountPayload = (req, res, next) => {
-
-  const errorMessage = { status: 400 }
-  const { name, budget } = req.body
+  const errorMessage = { status: 400 };
+  const { name, budget } = req.body;
   if (name === undefined || budget === undefined) {
-    errorMessage.message = 'name and budget are required'
-  next(errorMessage)
+    errorMessage.message = "name and budget are required";
+    next(errorMessage);
+  } else if (typeof name !== "string") {
+    errorMessage.message = "name of account must be a string";
+    next(errorMessage);
+  } else if (name.trim().length < 3 || name.trim().length > 100) {
+    errorMessage.message = "name of account must be between 3 and 100";
+    next(errorMessage);
+  } else if (typeof budget !== "number" || isNaN(budget)) {
+    errorMessage.message = "budget of account must be a number";
+    next(errorMessage);
+  } else if (budget < 0 || budget > 1000000) {
+    errorMessage.message = "budget of account is too large or too small";
+    next(errorMessage);
   }
-}
+};
 
 exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
-  console.log('checkAccountNameUnique middleware')
-next()
-}
+  console.log("checkAccountNameUnique middleware");
+  next();
+};
 
 exports.checkAccountId = async (req, res, next) => {
   try {
-    const account = await Account.getById(req.params.id)
+    const account = await Account.getById(req.params.id);
     if (!account) {
-      next({ status: 404, message: 'account not found'})
+      next({ status: 404, message: "account not found" });
     } else {
-      req.account = account
-      next()
+      req.account = account;
+      next();
     }
   } catch (err) {
-    next(err)
-} 
-}
+    next(err);
+  }
+};
